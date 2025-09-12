56d4e6dc22548ec0534f61b3de8fb9eb58e1825c
warubert <william.rubert@gmail.com>
2025-09-12 16:16:23 -0300

    env

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,7 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1;");
   response.status(200).json({ status: "ok" });
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,5 +1,4 @@
-test("GET to /api/v1/status deve retornar 200", () => {
-  fetch("http://localhost:3000/api/v1/status").then((response) => {
-    expect(response.status).toBe(200);
-  });
+test("GET to /api/v1/status deve retornar 200", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  expect(response.status).toBe(200);
 });
