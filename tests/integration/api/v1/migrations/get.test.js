341cdefd9412bcc24188bf8e8b3a984d6ffd1792
warubert <william.rubert@gmail.com>
2025-09-23 23:01:00 -0300

    refactor: refatora orchestrator criando clearDatabase()

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,9 +1,8 @@
-import database from "infra/database";
 import orchestrator from "tests/orchestrator.js";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
-  await database.query("DROP schema public cascade; create schema public;");
+  await orchestrator.clearDatabase();
 });
 
 describe("GET /api/v1/migrations", () => {
