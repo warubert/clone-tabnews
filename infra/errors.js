f947576f7d3901aaccea4881f681ca4b20a6f6ca
warubert <william.rubert@gmail.com>
2025-09-28 20:53:41 -0300

    feat: cria novo endpoint de Get para user

--- infra/errors.js
+++ infra/errors.js
@@ -76,3 +76,23 @@ export class ValidationError extends Error {
     };
   }
 }
+
+export class NotFoundError extends Error {
+  constructor({ cause, message, action }) {
+    super(message || "Não foi possível encontrar esse recurso no sistema", {
+      cause,
+    });
+    this.name = "NotFoundError";
+    this.action = action || "Verifique os parâmetros da consulta";
+    this.statusCode = 404;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
