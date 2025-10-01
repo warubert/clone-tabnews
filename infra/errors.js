cb79a41e6d995c1fc42ebaa79bdbeef99f982070
warubert <william.rubert@gmail.com>
2025-10-01 00:09:01 -0300

    feat: adiciona sessions

--- infra/errors.js
+++ infra/errors.js
@@ -96,3 +96,23 @@ export class NotFoundError extends Error {
     };
   }
 }
+
+export class UnauthorizedError extends Error {
+  constructor({ cause, message, action }) {
+    super(message || "Usuário não autenticado.", {
+      cause,
+    });
+    this.name = "UnauthorizedError";
+    this.action = action || "Refaça o login.";
+    this.statusCode = 401;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
